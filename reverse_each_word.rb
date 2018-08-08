fbfed4b25457d80d70d44281dc276a84380bca31
Patricia Carter <patricia4194@gmail.com>
2018-08-08 00:18:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
+# def reverse_each_word(string)
+#   array = []
+#   s = string.split(' ')
+#   s.each do |word|
+#     array << word.reverse!
+#   end
+#   array.join(" ")
+# end
+
 def reverse_each_word(string)
   array = []
   s = string.split(' ')
